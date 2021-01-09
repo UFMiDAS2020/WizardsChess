ddf04b4e9d1c1b8f8d6cbb37075ac87e683bf5ac
Linkraft <jordan.smiths.games@gmail.com>
2021-01-09 00:11:53 -0500

    Animation and interaction syncing

---
+++ Assets/XRGrabNetworkInteractable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+
+public class XRGrabNetworkInteractable : XRGrabInteractable
+{
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        photonView.RequestOwnership();
+        base.OnSelectEntered(interactor);
+    }
+}
